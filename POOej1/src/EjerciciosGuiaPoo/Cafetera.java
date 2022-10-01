2a03314c746590fa5bfc7d44d2acfd6edb8f834b
jeffGuillen16 <marcoguillen97@outlook.com>
2022-10-01 16:17:42 -0300

    agrego cambio

---
+++ POOej1/src/EjerciciosGuiaPoo/Cafetera.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EjerciciosGuiaPoo;
+
+/**
+ *
+ * @author marco
+ */
+public class Cafetera {
+    private int capacidadMaxima;
+    private int cantidadActual;
+     int edad;
+
+    public Cafetera() {
+    }
+
+    public Cafetera(int capacidadMaxima, int cantidadActual) {
+        this.capacidadMaxima = capacidadMaxima;
+        this.cantidadActual = cantidadActual;
+    }
+
+    public int getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
+
+    public int getCantidadActual() {
+        return cantidadActual;
+    }
+
+    public void setCapacidadMaxima(int capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+    }
+
+    public void setCantidadActual(int cantidadActual) {
+        this.cantidadActual = cantidadActual;
+    }
+    
+    public void llenarCafetera(){
+        this.cantidadActual=this.capacidadMaxima;
+    }
+    
+    public void servirTaza(int)
+}
