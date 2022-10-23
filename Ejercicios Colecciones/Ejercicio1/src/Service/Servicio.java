223a5831ea366547708151d41630f254adcd035a
jeffGuillen16 <marcoguillen97@outlook.com>
2022-10-23 01:36:30 -0300

    Agrego ejercicio 2

--- Ejercicios Colecciones/Ejercicio1/src/Service/Servicio.java
+++ Ejercicios Colecciones/Ejercicio1/src/Service/Servicio.java
@@ -7,6 +7,7 @@ package Service;
 
 import Perros.Perro;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Servicio {
@@ -15,25 +16,45 @@ public class Servicio {
 
     //Por defecto
     public Servicio() {
-        this.Razas = new ArrayList();
+        this.Razas = new ArrayList(); //Inicializa el nuevo ArrayList
     }
 
     public void listado() {
-
         String rp = "";
         Scanner Scan = new Scanner(System.in);
 
         do {
-           
-                System.out.println("Ingrese una Raza de perro:");
-                String dato = Scan.nextLine();
-                Razas.add(new Perro(dato));
-                System.out.println("Desea ingresar otra Raza de perro  : S/N ");
-                rp = Scan.nextLine();
+            System.out.println("Ingrese una Raza de perro:");
+            String dato = Scan.nextLine();
+            Razas.add(new Perro(dato));//Añado a Lista
+            System.out.println("Desea ingresar otra Raza de perro  : S/N ");
+            rp = Scan.nextLine();
         } while (rp.equals("S"));
     }
 
+    //Muestro la lista de ArrayList
     public void mostrar() {
         System.out.println(Razas);
     }
+
+    public void buscarPerro() {
+        boolean valor = false;
+        Scanner Scan = new Scanner(System.in);
+        System.out.print("¿Que Raza de perro desea Eliminar? :");
+        String dato = Scan.nextLine();
+        Iterator<Perro> it = Razas.iterator();
+        while (it.hasNext()) {
+            if (it.next().getRaza().equals(dato)) {
+                it.remove();
+                valor = true;
+            }
+
+        }
+
+        if (valor == false) {
+            System.out.println("No , se encontro la raza");
+        }
+        this.mostrar();
+    }
+
 }
